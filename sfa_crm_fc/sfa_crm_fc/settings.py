c33c8eb86f603cc595c6b3ef233c3c72379db70e
p1-kawamura <kawamura@p1-intl.com>
2025-06-05 16:50:02 +0900

    com1650

--- sfa_crm_fc/sfa_crm_fc/settings.py
+++ sfa_crm_fc/sfa_crm_fc/settings.py
@@ -51,6 +51,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'sfa_crm_fc.urls'
@@ -118,6 +119,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 NUMBER_GROUPING = 3
 SESSION_SAVE_EVERY_REQUEST = True
