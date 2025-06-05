1eb25f3b1df460bcf8898b90fcfaf8885c48c136
p1-kawamura <kawamura@p1-intl.com>
2025-06-05 16:19:15 +0900

    com1619

--- sfa_crm_fc/sfa_crm_fc/settings.py
+++ sfa_crm_fc/sfa_crm_fc/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6zkevjks%5&*y!)4#s+kgby#xc6#25!nf8^0syy%#s2bu*s!vo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
