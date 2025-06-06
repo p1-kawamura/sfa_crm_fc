72f1bc856756c3d9a41c47fce8bfbf7af17d6347
p1-kawamura <kawamura@p1-intl.com>
2025-06-06 16:00:07 +0900

    com1600

--- sfa_crm_fc/sfa_crm_fc/settings.py
+++ sfa_crm_fc/sfa_crm_fc/settings.py
@@ -78,10 +78,24 @@ WSGI_APPLICATION = 'sfa_crm_fc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sfa_crm_fc',
+        'USER': 'root',
+        'PASSWORD': 'sfa_crm_fc_1921689180',
+        'HOST': '192.168.91.80',
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
