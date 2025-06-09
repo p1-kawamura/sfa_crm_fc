1bcf33dce784b8991b19cd3f3715fa9f8455eaf8
p1-kawamura <kawamura@p1-intl.com>
2025-06-09 10:32:37 +0900

    com1032

--- sfa_crm_fc/crm/admin.py
+++ sfa_crm_fc/crm/admin.py
@@ -5,10 +5,12 @@ from django.contrib.admin import ModelAdmin
 class A_Customer(ModelAdmin):
     model=Customer
     list_display = ["cus_id","sei","mei"]
+    search_fields=("cus_id")
 
 class A_Crm_action(ModelAdmin):
     model=Crm_action
     list_display = ["act_id","cus_id","tantou_id","day","type","text","alert_check"]
+    search_fields=("cus_id")
 
 
 admin.site.register(Customer,A_Customer)
